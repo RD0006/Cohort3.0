2f9b8fedb14dd5ada02eee30baee9699f2bfb021
RD0006 <radhikadn2022@gmail.com>
2026-05-19 18:10:47 +0530

    Updations

---
+++ Course/18-05-2026/script.js
@@ -0,0 +1,22 @@
+console.log("Hello JS")
+
+console.log("This is normal text")
+console.warn("This is warning")
+console.error("This is error")
+console.table(['A', 'B', 'C'])
+
+var a = 12
+console.log(a)
+
+a = 20
+console.log(a)
+
+var b = 10
+
+console.log("Value of a:", a, "and Value of b:", b)
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
