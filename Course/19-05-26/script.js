2178764558bd8bfcec5c84970c09cd92b942f557
RD0006 <radhikadn2022@gmail.com>
2026-05-19 22:53:41 +0530

    Updations

---
+++ Course/19-05-26/script.js
@@ -0,0 +1,182 @@
+var a = 100
+var b = 20
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+
+console.log(typeof(a))
+console.log(typeof("hello"))
+
+// alert("Heylloooooo")
+// var c = confirm("Are you sure?")
+// console.log(c)
+// var user_name = prompt('Enter your name')
+// console.log(user_name)
+
+var a = 10
+var b = "Radhika"
+var c = true
+console.log(a + b + c)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+
+// var num1 = prompt("Enter number 1")
+// var num2 = prompt("Enter number 2")
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+
+var a = "10"
+var b = "20"
+var c = "true"
+
+console.log(a * b)
+console.log(c && true)
+
+var a = '100'
+console.log(typeof(Number(a)))
+
+// b = Number(prompt("Enter number"))
+// console.log(typeof(b))
+
+var a = 12
+var b = String(a)
+console.log(b)
+console.log(typeof(b))
+
+// var a = Number(prompt("Enter number 1"))
+// var b = Number(prompt("Enter number 2"))
+// console.log(a + b)
+
+var a = 10
+a++
+console.log(a)
+a--
+console.log(a)
+
+
+var a = 10
+console.log(a++)
+console.log(++a)
+
+var a = 10
+console.log(a--)
+console.log(--a)
+
+var a = 10
+a += 5
+console.log(a)
+
+var a = 10
+var b = 10
+console.log(a == b)
+console.log(a === b)
+console.log(a != b)
+console.log(a !== b)
+
+b = "10"
+console.log(a == b)
+console.log(a != b)
+console.log(a === b)
+console.log(a !== b)
+
+b = 20
+console.log(a < b)
+console.log(a <= b)
+console.log(a > b)
+console.log(a >= b)
+
+var a = 10
+var b = 20
+var c = 30
+var d = 40
+console.log(a > b || c < d)
+console.log(a > b && c < d)
+
+if(10 < 15) {
+    console.log("Heyyyyyyyyy")
+}
+else {
+    console.log("Byeeeeeeeee")
+}
+
+// var age = Number(prompt("Enter your age"))
+
+age = 18
+console.log(age)
+
+if (age >= 18) {
+    console.log("You can vote")
+}
+else {
+    console.log("You cannot vote")
+}
+
+// var maths = Number(prompt("Enter marks"))
+// var phy = Number(prompt("Enter marks"))
+// var chem = Number(prompt("Enter marks"))
+
+// if ((maths + phy + chem) / 3 >= 85) {
+   // console.log("Distinction")
+// }
+// else {
+   //  console.log("Better luck next time")
+// }
+
+// var marks = 95
+
+/*
+marks = (maths + phy + chem) / 3
+
+if (marks >= 85) {
+    console.log("A+")
+}
+else if (marks >= 70) {
+    console.log("B+")
+}
+else if (marks >= 50) {
+    console.log("C+")
+}
+else if (marks >= 33) {
+    console.log("D")
+}
+else {
+    console.log("Fail")
+}
+*/
+
+// var gender = prompt("Enter gender")
+// var age = Number(prompt("Enter age"))
+
+/*
+console.log(age)
+
+if (gender == "F") {
+    if (age >= 18 && age <= 60) {
+        console.log("You will get Rs 1500 per month")
+    }
+    else {
+        console.log("You will not get freebies")
+    }
+}
+else {
+    console.log("You will not get freebies")
+}
+*/ 
+
+if (10) {
+    console.log("Hello")
+}
+else {
+    console.log("Not Hello")
+}
+
+console.log(10 > 5 ? "Hello" : "Bye")
